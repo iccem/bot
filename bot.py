29d2e2ea5078bde30b88b772cb33e1b8c020a3e8
lilF <delo5070@gmail.com>
2022-03-24 12:56:30 +0400

    changed bot

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import nltk
 import random
-import sklearn
+# import sklearn
 import clear_text
 import import_json
 
@@ -9,7 +9,7 @@ j = import_json.GetJSON()
 BOT_CONFIG = j.get_json()
 
 class Bot:
-    def is_matching(text1, text2):
+    def is_matching(self, text1, text2):
         text1 = c.normalize(text1)
         text2 = c.normalize(text2)
         distance = nltk.edit_distance(text1, text2)
@@ -18,32 +18,32 @@ class Bot:
             average_length = 1
         return distance / average_length
 
-    def get_intent(text): # understanding of intence
+    def get_intent(self, text): # understanding of intence
         all_intents = BOT_CONFIG['intents']
 
         for name, data in all_intents.items():
             for example in data['examples']:
-                if is_matching(text, example):
+                if self.is_matching(text, example):
                     return name
                 
-    def get_answer(intent):
+    def get_answer(self, intent):
         responses = BOT_CONFIG['intents'][intent]['responses']
         return random.choice(responses)
 
-    def bot(text):
-        intent = get_intent(text)
+    # def bot(text):
+    #     intent = self.get_intent(text)
 
-        if not intent:
-            # подключить модель машинногоо бучения
-            test = vectorizer.transform([text])
-            intent = model.predict(test)[0] # по Х предсказать у, т е классифицировать
+    #     if not intent:
+    #         # подключить модель машинногоо бучения
+    #         test = vectorizer.transform([text])
+    #         intent = model.predict(test)[0] # по Х предсказать у, т е классифицировать
 
-        print('Intent = ', intent)
+    #     print('Intent = ', intent)
 
-        if intent:
-            return get_answer(intent)
+    #     if intent:
+    #         return get_answer(intent)
 
 
-        failure_phrases = BOT_CONFIG('failure_phrases')
-        random.choice(failure_phrases)
+    #     failure_phrases = BOT_CONFIG('failure_phrases')
+    #     random.choice(failure_phrases)
     
